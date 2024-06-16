d36dc6ef6074d1cfe84ff25cadf769f08bc534c6
Muhammad Haroon <mharoon1326@gmail.com>
2024-06-16 16:54:46 +0500

    Updating Footer

--- main/components/main/Footer.tsx
+++ main/components/main/Footer.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import {
-  RxDiscordLogo,
   RxGithubLogo,
-  RxInstagramLogo,
-  RxTwitterLogo,
   RxLinkedinLogo,
 } from "react-icons/rx";
-
-import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
         <div className="w-full flex flex-col items-center justify-center m-auto">
             <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-                
-
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxGithubLogo />
@@ -31,16 +24,7 @@ const Footer = () => {
                         <span className="text-[15px] ml-[6px]">+92-3325623738</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <span className="text-[15px] ml-[6px]">mharoon1326@gmail.com</span>    
-                    </p>
-                </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                   <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                      
-                        <span className="text-[15px] ml-[6px]">Learning about me</span>    
+                        <span className="text-[15px] ml-[6px]">@ mharoon1326@gmail.com</span>    
                     </p>
                 </div>
             </div>
