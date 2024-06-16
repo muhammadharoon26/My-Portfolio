b4e8b5fe34837039d24e1574c6c849fc53b70363
Muhammad Haroon <mharoon1326@gmail.com>
2024-06-16 15:52:36 +0500

    Resume Update

--- main/components/main/Navbar.tsx
+++ main/components/main/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
       {showPdf && (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 flex justify-center items-center z-50">
           <iframe
-            src="/Resume (1).pdf"
+            src="/Resume - Muhammad Haroon.pdf"
             title="PDF Viewer"
             width="80%"
             height="80%"
