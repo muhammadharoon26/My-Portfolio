c7094dd62763866ca4a40d7407c5eecc69478404
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-16 16:27:38 +0500

    `Update HeroContent component with new content and descriptions`
    Update HeroContent component with new content and descriptions.

--- main/components/sub/HeroContent.tsx
+++ main/components/sub/HeroContent.tsx
@@ -16,7 +16,8 @@ const HeroContent = () => {
             className='Welcome-box py-[15px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
                 <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5"  />
                     <h1 className='Welcome-text text-bold text-[13px]'>
-                        Muhammad Haroon Portfolio
+                        {/* Muhammad Haroon Portfolio */}
+                        Developer with 10 months of experience in cloud, CI/CD and API development. Proficient in Python and database optimisation. Proven ability to design and implement scalable solutions, enhance application performance and drive successful projects in Agile environments. Committed to continuous learning and adapting evolving technologies.
                     </h1>
             </motion.div>
             <motion.div
@@ -24,14 +25,14 @@ const HeroContent = () => {
             className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
                 <span>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-lime-500 to-red-500'>
-                        Providing DevOps Consultation
+                        Delivering Solutions of DevOps | Backend Development | API Development | Web Scraping 
                     </span>
                 </span>
             </motion.div>
             <motion.p
             variants={slideInFromLeft(0.8)}
             className='text-lg text-purple-400 my-5 max-w-[600px]'>
-                DevOps Practitioner | Cyber Security Enthusiast
+                DevOps Engineer | Backend Developer
             </motion.p>
         </div>
         <motion.div
