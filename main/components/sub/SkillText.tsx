0fe630f44622ed30a5bcdbc87138a658f8b42521
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-16 16:55:48 +0500

    `Refactor SkillText component: update motion imports and layout`
    Refactor SkillText component: update motion imports and layout.

--- main/components/sub/SkillText.tsx
+++ main/components/sub/SkillText.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import { motion } from 'framer-motion'
-import { slideInFromLeft, slideInFromRight} from '@/utils/motion'
+import { slideInFromLeft, slideInFromRight } from '@/utils/motion'
 
 const SkillText = () => {
   return (
@@ -12,11 +12,20 @@ const SkillText = () => {
       >
         Providing Smooth Workflow of Your Product
       </motion.div>
-      <motion.div
+      {/* <motion.div
         variants={slideInFromLeft(0.5)}
         className='text-[25px] text-white font-medium mt-[10px] text-center mb-[15px]'
       >
         Developer with 10 months of experience in cloud, CI/CD and API development. Proficient in Python and database optimisation. Proven ability to design and implement scalable solutions, enhance application performance and drive successful projects in Agile environments. Committed to continuous learning and adapting evolving technologies.
+      </motion.div> */}
+      <motion.div
+        variants={slideInFromLeft(0.5)}
+        className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
+        <span>
+          <span className='text-transparent bg-clip-text bg-gradient-to-r from-lime-500 to-red-500'>
+            Developer with 10 months of experience in cloud, CI/CD and API development. Proficient in Python and database optimisation. Proven ability to design and implement scalable solutions, enhance application performance and drive successful projects in Agile environments. Committed to continuous learning and adapting evolving technologies.
+          </span>
+        </span>
       </motion.div>
       <motion.div
         variants={slideInFromRight(0.5)}
