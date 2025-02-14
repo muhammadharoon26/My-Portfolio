db9c5ccf3f2afdab64275b40deff589f373e6516
Muhammad Haroon <mharoon1326@gmail.com>
2025-02-14 18:47:58 +0500

    project thumbnail filename change

--- main/components/main/Projects.tsx
+++ main/components/main/Projects.tsx
@@ -59,13 +59,13 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full grid grid-cols-1 md:grid-cols-3 gap-10 px-10">
         <ProjectCard
-          src="/livechat-support.webp"
+          src="/livechat-support.png"
           title="Live Chat Support System"
           description="A real-time chat support system built with Go, featuring WebSocket communication, JWT authentication, and message persistence using SQLite and Redis."
           githubLink="https://github.com/muhammadharoon26/livechat-support"
         />
         <ProjectCard
-          src="/task_manager_api.webp"
+          src="/task_manager_api.png"
           title="Task Manager API"
           description="A robust REST API for managing tasks with real-time updates using Socket.IO, built with Node.js, Express, and MongoDB."
           githubLink="https://github.com/muhammadharoon26/task-manager-api"
